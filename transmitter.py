a8a52d80f1437591f89167b88e74e99578b123e8
kartboy500 <kartboy500@gmail.com>
2017-02-19 02:14:34 -0600

    Finished upload and download

--- transmitter.py
+++ transmitter.py
@@ -1,37 +1,56 @@
-# Dependencies: pywin32, boto3
-# https://sourceforge.net/projects/pywin32/files/pywin32/
+# Dependencies: boto3
 # pip install boto3
 
+import time
 import os
 import datetime
-import win32file
-import win32event
-import win32con
 import boto3
 
+# SET ACCORDING TO UE4 SAVE LOCATION
 savefilepath = os.path.abspath('.')
-savefilename = 'jengastate.sav'
+savefilename = os.path.abspath('./jengastate.sav')
 s3 = boto3.resource('s3')
 bucket = 'jengalongdistance'
 gameidkey = 'game1'
+checkperiod = 15 # seconds
+# to prevent infinite upload/download loop
+margin = 30 # seconds
 
 def main():
-    change_handle = win32file.FindFirstChangeNotification(savefilepath, 0, win32con.FILE_NOTIFY_CHANGE_LAST_WRITE)
-
-    try:
-        while True:
-            # 500 ms timeout
-            result = win32event.WaitForSingleObject(change_handle, 500)
+    while True:
+        cloud_file = s3.Object(bucket, gameidkey)
+        cloud_mtime = cloud_file.last_modified
+        local_mtime = datetime.datetime.replace(datetime.datetime.fromtimestamp(os.path.getmtime(savefilename)), tzinfo=datetime.timezone(datetime.timedelta(hours=-6)))
+        print()
+        print('Cloud', cloud_mtime)
+        print('Local', local_mtime)
+        print('Cloud - local', cloud_mtime - local_mtime)
+        print('Local - cloud', local_mtime - cloud_mtime)
+        print('Delta', datetime.timedelta(seconds = margin))
+        print('l - c > delta', (local_mtime - cloud_mtime) > datetime.timedelta(seconds = margin))
+        print('c - l > delta', (cloud_mtime - local_mtime) > datetime.timedelta(seconds = margin))
+        
+        # local check
+        if local_mtime > cloud_mtime and (local_mtime - cloud_mtime) > datetime.timedelta(seconds = margin):
+            uploadsave()
             
-            if result == win32con.WAIT_OBJECT_0:
-                uploadsave()
-                win32file.FindNextChangeNotification(change_handle)
-    finally:
-        win32file.FindCloseChangeNotification(change_handle)
+        # cloud check
+        if cloud_mtime > local_mtime and (cloud_mtime - local_mtime) > datetime.timedelta(seconds = margin):
+            downloadsave()
+        
+        # check period
+        time.sleep(checkperiod)
       
 def uploadsave():
     data = open(savefilename, 'rb')
     s3.Bucket(bucket).put_object(Key=gameidkey, Body=data)
-    print(savefilename, ' in ', os.listdir(savefilepath), ' uploaded at ', datetime.datetime.now())
-      
+    data.close()
+    print(savefilename, 'uploaded at', datetime.datetime.now())
+
+def downloadsave():
+    data = open(savefilename, 'wb')
+    s3.Object(bucket, gameidkey).download_fileobj(data)
+    data.close()
+    print(savefilename, 'downloaded at', datetime.datetime.now())
+    
 main()
