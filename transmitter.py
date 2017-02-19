2dbe9dc5d9adf8d63cd9b4331f40b488bf6024e6
kartboy500 <kartboy500@gmail.com>
2017-02-18 23:35:47 -0600

    Added AWS put functionality

--- transmitter.py
+++ transmitter.py
@@ -1,14 +1,19 @@
-# Dependencies: pywin32, requests
+# Dependencies: pywin32, boto3
 # https://sourceforge.net/projects/pywin32/files/pywin32/
-# pip install requests
+# pip install boto3
+
 import os
 import datetime
-
 import win32file
 import win32event
 import win32con
+import boto3
 
 savefilepath = os.path.abspath('.')
+savefilename = 'jengastate.sav'
+s3 = boto3.resource('s3')
+bucket = 'jengalongdistance'
+gameidkey = 'game1'
 
 def main():
     change_handle = win32file.FindFirstChangeNotification(savefilepath, 0, win32con.FILE_NOTIFY_CHANGE_LAST_WRITE)
@@ -25,6 +30,8 @@ def main():
         win32file.FindCloseChangeNotification(change_handle)
       
 def uploadsave():
-    print(os.listdir(savefilepath), ' updated at ', datetime.datetime.now())
+    data = open(savefilename, 'rb')
+    s3.Bucket(bucket).put_object(Key=gameidkey, Body=data)
+    print(savefilename, ' in ', os.listdir(savefilepath), ' uploaded at ', datetime.datetime.now())
       
 main()
